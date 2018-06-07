39c31370d91a9a415a411a6702fa6aca0933c41b
Andreas Spielhofer <andreas.spielhofer@mail.mcgill.ca>
2018-06-07 18:48:13 -0400

    Update MGI.py

--- MGI.py
+++ MGI.py
@@ -1,3 +1,13 @@
+"""
+Created on Tue Mar 08 14:45:27 2015
+@author:    Andreas Spielhofer
+            Ph.D. Candidate
+            Physics Departement
+            McGill University
+            Montreal, Canada
+@contact:   andreas.spielhofer@mail.mcgill.ca
+"""
+
 from spinmob import egg
 import spinmob
 import Keithley as _k
