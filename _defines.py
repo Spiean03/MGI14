ffd0ae26699792384737374468056bbf5602672d
Andreas Spielhofer <andreas.spielhofer@mail.mcgill.ca>
2018-06-07 18:48:35 -0400

    Update _defines.py

--- _defines.py
+++ _defines.py
@@ -1,3 +1,13 @@
+"""
+Created on Tue Mar 08 14:45:27 2015
+@author:    Andreas Spielhofer
+            Ph.D. Candidate
+            Physics Departement
+            McGill University
+            Montreal, Canada
+@contact:   andreas.spielhofer@mail.mcgill.ca
+"""
+
 # Serial and Baudrate for the Kleindiek MM3A Micromanipulator
 DEFAULT_SERIAL = 'COM20'
 DEFAULT_BAUDRATE = 115200
